16c76e15674dd5a9d71474bb6a04675d7476513e
Kevin Nilsson <kevin_nilsson98@hotmail.com>
2014-12-21 22:20:25 +0100

    Create server.lua
    This is completely unfinished, it's just to get the idea what the server will do later on.
    I'll create a database API for this later

---
+++ server.lua
@@ -0,0 +1,46 @@
+--[[
+    [Program] ByteBank Server
+    @version 1.0, 2014-12-21
+    @author TheOddByte
+--]]
+
+local path = ".ByteBank-Server"
+local public_key = "Keystone"
+
+--# Make sure that the source folder exists
+if not exists( path ) then
+    fs.makeDir( path )
+    local directories = {
+        "APIs", "Scripts";
+    }
+    for _, name in ipairs( directories ) do
+        fs.makeDir( path .. "/" .. name )
+    end
+    --# Download the required APIs and scripts here
+end
+
+--# Load the APIs
+for _, name in ipairs( fs.list( path .. "/APIs/" ) do
+    os.loadAPI( path .. "/APIs/" .. name )
+end
+
+--# Load the scripts
+local SNet = dofile( path .. "/Scripts/SNet" )
+
+
+local function main()
+    local server = SNet.host( "ByteBank", "server", public_key )
+    if not server then
+        error( "Failed to host server", 0 )
+    end
+    while true do
+        local e = { os.pullEvent() }
+        server:handle( unpack( e ) )
+    end
+end
+
+
+local ok, err = pcall( main )
+if not ok and err ~= "Terminated" then
+    -- Crash screen here
+end
